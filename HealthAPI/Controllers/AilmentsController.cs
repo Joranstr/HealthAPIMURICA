91d204d49b0441dc44d20c2363d2305e0bf31c6d
Joakim Nikolai Schrøder <55440505+Schrodern93@users.noreply.github.com>
2020-05-06 13:16:54 +0200

    sfgs

---
+++ HealthAPI/Controllers/AilmentsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HealthAPI.ViewModel;
+using HealthSQLDB.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AilmentsController : ControllerBase
+    {
+        private readonly HealthContext _context;
+
+        public AilmentsController(HealthContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Patients
+        [HttpGet]
+        public async Task<ActionResult<List<AilmentViewModel>>> GetAilments()
+        {
+            var Ailments = await _context.Ailments.ToArrayAsync();
+            
+               
+            return Ailments.Select(a => new AilmentViewModel{Id = a.Id, Name=a.Name }).ToList();
+        }
+    }
+}
---
+++ HealthAPI/Controllers/MedicationsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HealthAPI.ViewModel;
+using HealthSQLDB.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicationsController : ControllerBase
+    {
+        private readonly HealthContext _context;
+
+        public MedicationsController(HealthContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Patients
+        [HttpGet]
+        public async Task<ActionResult<List<MedicationViewModel>>> GetMedications()
+        {
+            var Medications = await _context.Medications.ToArrayAsync();
+
+
+            return Medications.Select(a => new MedicationViewModel { Id = a.Id, Name = a.Name }).ToList();
+        }
+    }
+}
