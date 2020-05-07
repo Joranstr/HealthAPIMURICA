090a381b9aa7a90908b9f58411f2453f4e51dcaf
Joakim Nikolai Schrøder <55440505+Schrodern93@users.noreply.github.com>
2020-05-07 14:59:18 +0200

    adf

--- HealthAPI/Controllers/PatientsController.cs
+++ HealthAPI/Controllers/PatientsController.cs
@@ -10,130 +10,150 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HealthAPI.Controllers
 {
-   
-        [Route("api/[controller]")]
-        [ApiController]
-        //[EnableCors("HealthPolicy")]
-        public class PatientsController : ControllerBase
+
+    [Route("api/[controller]")]
+    [ApiController]
+    //[EnableCors("HealthPolicy")]
+    public class PatientsController : ControllerBase
+    {
+        private readonly HealthContext _context;
+
+        public PatientsController(HealthContext context)
         {
-            private readonly HealthContext _context;
+            _context = context;
+        }
 
-            public PatientsController(HealthContext context)
-            {
-                _context = context;
-            }
-
-            // GET: api/Patients
-            [HttpGet]
-            public async Task<ActionResult<List<PatientViewModel>>> GetPatients()
-            {
+        // GET: api/Patients
+        [HttpGet]
+        public async Task<ActionResult<List<PatientViewModel>>> GetPatients()
+        {
             var Patients = await _context.Patients
                 .Include(pa => pa.PatientAilments)
                  .ThenInclude(pa => pa.Ailment)
                 .Include(pm => pm.PatientMedications)
                 .ThenInclude(pm => pm.Medication)
                   .ToListAsync();
-                return Patients.Select(p => new PatientViewModel(p)).ToList();
+            return Patients.Select(p => new PatientViewModel(p)).ToList();
 
-            }
-
-            // GET: api/Patients/5
-            [HttpGet("{id}")]
-            public async Task<ActionResult<PatientViewModel>> GetPatient(int id)
-            {
-                var patient = await _context.Patients
-                  .Include(pa => pa.PatientAilments)
-                   .ThenInclude(pa => pa.Ailment)
-                  .Include(pm => pm.PatientMedications)
-                    .ThenInclude(pm => pm.Medication)
-                    .FirstOrDefaultAsync(i => i.PatientId == id);
-
-                if (patient == null)
-                {
-                    return NotFound();
-                }
-
-                return new PatientViewModel(patient);
-            }
-
-            // PUT: api/Patients/5
-            // To protect from overposting attacks, enable the specific properties you want to bind to, for
-            // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-            [HttpPut("{id}")]
-            public async Task<IActionResult> PutPatient(int id, Patient patient)
-            {
-                if (id != patient.PatientId)
-                {
-                    return BadRequest();
-                }
-
-                _context.Entry(patient).State = EntityState.Modified;
-
-                try
-                {
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PatientExists(id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-
-                return NoContent();
-            }
-
-            // POST: api/Patients
-            // To protect from overposting attacks, enable the specific properties you want to bind to, for
-            // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-            [HttpPost]
-            public async Task<ActionResult<PatientViewModel>> PostPatient(PatientViewModel patient)
-            {
-                //var patient = new PatientViewModel(new Patient());
-                var newPatient = patient.ConvertToPatient();
-                _context.Patients.Add(newPatient);
-                await _context.SaveChangesAsync();
-                
-                return new PatientViewModel(newPatient); 
-                //CreatedAtAction("GetPatient", new { id = patient.PatientId }, patient);
-            }
-
-            // DELETE: api/Patients/5
-            [HttpDelete("{id}")]
-            public async Task<ActionResult<Patient>> DeletePatient(int id)
-            {
-                var patient = await _context.Patients.FindAsync(id);
-                if (patient == null)
-                {
-                    return NotFound();
-                }
-
-                _context.Patients.Remove(patient);
-                await _context.SaveChangesAsync();
-
-                return patient;
-            }
-
-            private bool PatientExists(int id)
-            {
-                return _context.Patients.Any(e => e.PatientId == id);
-            }
-
-            [HttpGet("{id:int}/medication")]
-            public async Task<IActionResult> GetMedications(int id)
-            {
-                var patient = await _context.Patients
-                  
-                    .FirstOrDefaultAsync(i => i.PatientId == id);
-
-                if (patient == null) return NotFound();
-                return Ok(patient);
-            }
         }
+
+        // GET: api/Patients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PatientViewModel>> GetPatient(int id)
+        {
+            var patient = await _context.Patients
+              .Include(pa => pa.PatientAilments)
+               .ThenInclude(pa => pa.Ailment)
+              .Include(pm => pm.PatientMedications)
+                .ThenInclude(pm => pm.Medication)
+                .FirstOrDefaultAsync(i => i.PatientId == id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return new PatientViewModel(patient);
+        }
+
+        // PUT: api/Patients/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPatient(int id, Patient patient)
+        {
+            if (id != patient.PatientId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(patient).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PatientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Patients
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<PatientViewModel>> PostPatient(PatientViewModel patient)
+        {
+
+            var newPatient = patient.ConvertToPatient();
+            _context.Patients.Add(newPatient);
+            await _context.SaveChangesAsync();
+
+            newPatient.PatientAilments = new List<PatientAilments>();
+            newPatient.PatientMedications = new List<PatientMedications>();
+
+            foreach (var ailment in patient.Ailments)
+            {
+                var Ailment = await _context.Ailments.FirstOrDefaultAsync(a => a.Id == ailment.Id);
+                var patientAilment = new PatientAilments() {Patient = newPatient, PatientId = newPatient.PatientId, Ailment = Ailment, AilmentId = Ailment.Id };
+                newPatient.PatientAilments.Add(patientAilment);
+            }
+
+            foreach (var medication in patient.Medications)
+            {
+                var Medication = await _context.Medications.FirstOrDefaultAsync(m => m.Id == medication.Id);
+                var patientMedication = new PatientMedications() { Patient = newPatient, PatientId = newPatient.PatientId, Medication = Medication, MedicationId = Medication.Id };
+                newPatient.PatientMedications.Add(patientMedication);
+            }
+
+            _context.Patients.Update(newPatient);
+            await _context.SaveChangesAsync();
+
+            return new PatientViewModel(newPatient);
+            //CreatedAtAction("GetPatient", new { id = patient.PatientId }, patient);
+        }
+
+        // DELETE: api/Patients/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Patient>> DeletePatient(int id)
+        {
+            var patient = await _context.Patients.FindAsync(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            _context.Patients.Remove(patient);
+            await _context.SaveChangesAsync();
+
+            return patient;
+        }
+
+        private bool PatientExists(int id)
+        {
+            return _context.Patients.Any(e => e.PatientId == id);
+        }
+
+        [HttpGet("{id:int}/medication")]
+        public async Task<IActionResult> GetMedications(int id)
+        {
+            var patient = await _context.Patients
+
+                .FirstOrDefaultAsync(i => i.PatientId == id);
+
+            if (patient == null) return NotFound();
+            return Ok(patient);
+        }
+    }
 
 }
